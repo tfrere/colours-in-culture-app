ca11b7f867d671882c6a82b0a99bdc8a864c9b15
thibaud frere <tfrere@student.42.fr>
2019-05-02 13:56:29 +0200

    push

--- src/main.js
+++ src/main.js
@@ -1,49 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import Multiselect from 'vue-multiselect'
-import vueHeadful from 'vue-headful';
-import VueMasonry from 'vue-masonry-css'
-import ToolTip from 'vue-directive-tooltip'
-import 'vue-directive-tooltip/css/index.css';
-import VueResize from 'vue-resize'
-import VueCookie from 'vue-cookie'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Multiselect from "vue-multiselect";
+import vueHeadful from "vue-headful";
+import VueMasonry from "vue-masonry-css";
+import ToolTip from "vue-directive-tooltip";
+import "vue-directive-tooltip/css/index.css";
+import VueResize from "vue-resize";
+import VueCookie from "vue-cookie";
 // register globally
-Vue.component('vue-headful', vueHeadful);
-Vue.component('multiselect', Multiselect);
+Vue.component("vue-headful", vueHeadful);
+Vue.component("multiselect", Multiselect);
 Vue.use(ToolTip);
 Vue.use(VueMasonry);
-Vue.use(VueResize)
+Vue.use(VueResize);
 Vue.use(VueCookie);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  mounted: () => document.dispatchEvent(new Event("x-app-rendered"))
+}).$mount("#app");
 
-
-// // Step 2: Create a renderer
-// const renderer = require('vue-server-renderer').createRenderer()
-//
-// // Step 3: Render the Vue instance to HTML
-// renderer.renderToString(Vue, (err, html) => {
-//   if (err) throw err
-//   console.log(html)
-//   // => <div data-server-rendered="true">Hello World</div>
-// })
-//
-// // in 2.5.0+, returns a Promise if no callback is passed:
-// renderer.renderToString(app).then(html => {
-//   console.log(html)
-// }).catch(err => {
-//   console.error(err)
-// });
-
-
-
-Vue.directive('title', {
-  inserted: (el, binding) => document.title = binding.value,
-  update: (el, binding) => document.title = binding.value
+Vue.directive("title", {
+  inserted: (el, binding) => (document.title = binding.value),
+  update: (el, binding) => (document.title = binding.value)
 });
