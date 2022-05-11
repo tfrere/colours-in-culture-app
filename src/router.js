77964ecb2b56d6f60247dd58f10fcaac6d437f07
tfrere <frere.thibaud@gmail.com>
2022-05-11 12:03:47 +0200

    update

--- src/router.js
+++ src/router.js
@@ -1,47 +1,45 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
-import ByConcept from '@/pages/ByConcept.vue';
-import ByColor from '@/pages/ByColor.vue';
-import ByCulture from '@/pages/ByCulture.vue';
-import Home from '@/pages/Home.vue';
-
+import ByConcept from "@/pages/ByConcept.vue";
+import ByColor from "@/pages/ByColor.vue";
+import ByCulture from "@/pages/ByCulture.vue";
+import Home from "@/pages/Home.vue";
 
 const router = new Router({
-  mode: 'history',
+  mode: "hash",
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
       // redirect: '/culture/westernAmerican'
     },
     {
-      path: '/culture/:culture?',
-      name: 'culture',
-      title: 'Culture',
-      component: ByCulture
+      path: "/culture/:culture?",
+      name: "culture",
+      title: "Culture",
+      component: ByCulture,
     },
     {
-      path: '/color/:color?',
-      name: 'color',
-      title: 'Color',
-      component: ByColor
+      path: "/color/:color?",
+      name: "color",
+      title: "Color",
+      component: ByColor,
     },
     {
-      path: '/concept/:concept?',
-      name: 'concept',
-      title: 'Concept',
-      component: ByConcept
-    }
-  ]
+      path: "/concept/:concept?",
+      name: "concept",
+      title: "Concept",
+      component: ByConcept,
+    },
+  ],
 });
 
 router.beforeEach(function(to, from, next) {
-    next();
-})
+  next();
+});
 
-
-export default router
+export default router;
